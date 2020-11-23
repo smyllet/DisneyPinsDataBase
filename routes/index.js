73ce5acc0d68585248f5fea381f812e1a6710e3b
smyllet <smyllet.gamer@gmail.com>
2020-11-23 15:25:43 +0100

    Ajout du support de l'anglais + Ajout d'un fichier lang.json contenant tout les text du site + Ajout de la detection de la langue du navigateur

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
 /* Get Index Page. */
 router.get('/', function(req, res, next) {
+    let navLang = req.acceptsLanguages( 'fr', 'en')
+    if(!navLang) navLang = 'fr'
     res.render('pages/index.ejs', {
-        title: "accueil"
+        title: "accueil",
+        navLang : navLang
     })
-});
+})
 
 module.exports = router;
